171767ee6060ef6372a816cec6e75a2abefe9d52
jto <jto@zenexity.com>
2011-09-14 10:57:45 +0200

    index

--- app/controllers.scala
+++ app/controllers.scala
@@ -6,9 +6,14 @@ import play.api.mvc.Results._
 object Application extends Controller {
     import scalaz._
     import Scalaz._
-    
+        
+    // TODO: do we want to get rid of this?
+    // Are controllers supposed to stay pure?
     def index = Action {
         import java.util.Date
-        Text(new Date().toString)
+        // TODO: We can't use implicit conversion here because Html will expect an implicit Writeable
+        // -- implicit def html2String(h:play.templates.Html) = h.toString
+        // We either have to call toString explicitly, or provide a Writeable for play.templates.Html
+        Html(views.Application.html.index(new Date()).toString)
     }
 }
