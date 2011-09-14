d857a52c22a195d9bf5adc02a5c3a7b0fdf323a1
jto <jto@zenexity.com>
2011-09-14 11:07:00 +0200

    implicit writeable for Html

--- app/controllers.scala
+++ app/controllers.scala
@@ -6,14 +6,16 @@ import play.api.mvc.Results._
 object Application extends Controller {
     import scalaz._
     import Scalaz._
-        
+    
+    
+    implicit val wHtml : Writeable[play.templates.Html] = AsString[play.templates.Html](_.toString)
+    
     // TODO: do we want to get rid of this?
     // Are controllers supposed to stay pure?
     def index = Action {
         import java.util.Date
-        // TODO: We can't use implicit conversion here because Html will expect an implicit Writeable
-        // -- implicit def html2String(h:play.templates.Html) = h.toString
+        // We can't use implicit conversion here because Html will expect an implicit Writeable
         // We either have to call toString explicitly, or provide a Writeable for play.templates.Html
-        Html(views.Application.html.index(new Date()).toString)
+        Html(views.Application.html.index(new Date()))
     }
 }
