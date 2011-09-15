f96d1eae144f7ef6d78c4ecf5294e5ebf001ea8a
jto <boudhevil@gmail.com>
2011-09-16 00:36:09 +0200

    QueryStringBindable[Option[T]]

--- app/controllers.scala
+++ app/controllers.scala
@@ -13,7 +13,17 @@ package object controllers{
             None
       )))
     }
-    def unbind(key:String, value:Contact) = "?contact.id=42"
+    
+    //TODO: urlencode
+    def unbind(key:String, c:Contact) = List("id", "firstname", "name", "birthdate", "email")
+                                            .zip(List(c.id, c.firstname, c.name, c.birthdate, c.email))
+                                            .map(t => key + '.' + t._1 + "=" + t._2)
+                                            .reduce(_ + "&" + _)
+  }
+  
+  implicit def bindableOption[T : QueryStringBindable] = new QueryStringBindable[Option[T]] {
+    def bind(key:String, params:Map[String,Seq[String]]) = implicitly[QueryStringBindable[T]].bind(key, params).map(_.right.map(Option(_)))
+    def unbind(key:String, value:Option[T]) = value.map(implicitly[QueryStringBindable[T]].unbind(key, _)).getOrElse("")
   }
 }
 
@@ -37,9 +47,15 @@ package controllers{
       def save(id: Int) = Action { ctx: Context =>
         Text(ctx.request.toString)
       }
-    
-      def create(contact: Contact) = Action {
-        save(0)
+      
+      def testOption(i: Option[Int]) = Action {
+        Text(i.toString)
+        //save(0)
+      }
+      
+      def create(contact: Option[Contact]) = Action {
+        Text(contact.toString)
+        //save(0)
       }
   }
 }
